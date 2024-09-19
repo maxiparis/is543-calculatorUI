9d401da0c03bd6836b412fb82a337b760566687b
Maximiliano Paris <mparis@hotmail.cl>
2024-09-18 22:04:45 -0600

    feat: finished the ui

--- CalculatorUI/ContentView.swift
+++ CalculatorUI/ContentView.swift
@@ -9,18 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             Spacer()
             HStack {
                 Spacer()
-                Text("1324.4") //Just a placeholder
-                    .font(.largeTitle)
+                Text("12345") //Just a placeholder
+                    .font(.system(size: 50, weight: .semibold))
+            }.padding(.bottom, 20)
+            HStack(spacing: 10) {
+                OperationButton(symbol: "C")
+                OperationButton(symbol: "+")
+                OperationButton(symbol: "-")
+                OperationButton(symbol: "/")
             }
-            Spacer()
+            HStack {
+                NumberButton(symbol: "7")
+                NumberButton(symbol: "8")
+                NumberButton(symbol: "9")
+                OperationButton(symbol: "*")
+            }
+            HStack {
+                NumberButton(symbol: "4")
+                NumberButton(symbol: "5")
+                NumberButton(symbol: "6")
+                OperationButton(symbol: "=")
+            }
+            HStack {
+                NumberButton(symbol: "1")
+                NumberButton(symbol: "2")
+                NumberButton(symbol: "3")
+                OperationButton(symbol: "0")
+            }
         }.padding(40)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct NumberButton: View {
+    var symbol: String
+    var body: some View {
+        HStack(spacing: 10) {
+            Button {
+                
+            } label: {
+                Text(symbol)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .frame(width: 80, height: 80)
+            }
+            .background(Color.blue)
+            .cornerRadius(50)
+        }
+    }
+}
+
+struct OperationButton: View {
+    var symbol: String
+    var body: some View {
+        HStack(spacing: 10) {
+            Button {
+                
+            } label: {
+                Text(symbol)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.black)
+                    .frame(width: 80, height: 80)
+            }
+            .background(Color.cyan)
+            .cornerRadius(50)
+        }
+    }
 }
