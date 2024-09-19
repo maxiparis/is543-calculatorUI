f74985cab31c7cb273adfe3a51e4e5d65a44ae3c
Maximiliano Paris <mparis@hotmail.cl>
2024-09-18 21:36:29 -0600

    Initial Commit

---
+++ CalculatorUI/CalculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUIApp.swift
+//  CalculatorUI
+//
+//  Created by Maximiliano París Gaete on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
