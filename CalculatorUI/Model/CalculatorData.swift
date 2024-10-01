3d8e7dc4c4e414d4d0da8f4e6fd4c755a836733e
Maximiliano Paris <mparis@hotmail.cl>
2024-09-30 22:30:26 -0600

    feat: user intents/ stuff

---
+++ CalculatorUI/Model/CalculatorData.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorData.swift
+//  CalculatorUI
+//
+//  Created by Maximiliano París Gaete on 9/30/24.
+//
+
+import Foundation
+
+struct CalculatorData {
+    
+}
--- CalculatorUI/ViewModel/CalculatorBrain.swif.swift
+++ CalculatorUI/ViewModel/CalculatorBrain.swif.swift
@@ -10,5 +10,18 @@ import Foundation
 
 
 class CalculatorBrain: ObservableObject {
+    //MARK: - Variables
+    
     @Published var soundOn: Bool = false
+    private var model = CalculatorData()
+    
+    //MARK: - User intents
+    func numberTapped(_ number: String) {
+        //TODO
+    }
+    
+    func functionTapped(_ function: String) {
+        //TODO
+    }
+
 }
